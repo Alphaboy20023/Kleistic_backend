2e7d4a2ae73bbd61231a21df39006d9459acc7fe
Alphaboy20023 <timivictor565@gmail.com>
2025-06-27 01:16:30 +0200

    changes

--- kelistic_app/serializers.py
+++ kelistic_app/serializers.py
@@ -35,7 +35,6 @@ class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
     
-    user = authenticate(username=username, password=password)
 
     def validate(self, data):
         username = data.get('username')
